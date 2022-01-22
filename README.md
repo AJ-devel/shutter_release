05bd8d672c9f214364e42a08c423a0aad993a2a4
RobeGuy <aacajo@gmail.com>
2022-01-22 09:45:16 -0700

    Added script

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Description:
 
 This script is intended to be used with older DSLR cameras that don't have the ability to control the shutter over USB.  A modified shutter release cable connected to a USB relay is required for this to run.  The camera must also be in BULB mode.
+
+## Run example:
+
+.\serial_relay_shutter_control.ps1 -com_port_number 8 -baud_rate 9600 -exposure_time_seconds 120 -number_of_pictures 10
+
+The above example will connect to COM7 at baud 9600 taking a 2 minute exposure waiting 1 second between exposures.  It will take a total of 10 pictures like this.
---
+++ serial_relay_shutter_control.ps1
@@ -0,0 +1,86 @@
+﻿param(
+        [Parameter()]
+        [int]$com_port_number,
+
+        [Parameter()]
+        [int]$baud_rate,
+
+        [Parameter()]
+        [int]$exposure_time_seconds,
+
+        [Parameter()]
+        [int]$number_of_pictures
+)
+
+
+
+function Open-Shutter {
+    param(
+        [Parameter()]
+        [System.IO.Ports.SerialPort] $port,
+
+        [Parameter()]
+        [Byte[]] $open_relay_hex
+    )
+
+    $port.open()
+    $port.Write($open_relay_hex, 0, $open_relay_hex.Count)
+    $port.close()
+
+    Write-Host "Shutter open"
+    }
+
+
+function Close-Shutter {
+    param(
+        [Parameter()]
+        [System.IO.Ports.SerialPort] $port,
+
+        [Parameter()]
+        [Byte[]] $close_relay_hex
+    )
+
+    $port.open()
+    $port.Write($close_relay_hex, 0, $close_relay_hex.Count)
+    $port.close()
+
+    Write-Host "Shutter closed"
+    }
+
+
+$port = New-Object System.IO.Ports.SerialPort("COM$com_port_number", $baud_rate, "None", 8, "One")
+
+# Values for E-3 USB Serial Relay CH340 - This may differ if using a different relay model
+[Byte[]] $open_relay_hex = 0xA0,0x01,0x01,0xA2
+[Byte[]] $close_relay_hex = 0xA0,0x01,0x00,0xA1
+
+
+try {
+        $i = 0
+
+        While ($number_of_pictures -gt 0)
+        {
+            $number_of_pictures--
+
+            Open-Shutter $port $open_relay_hex
+            Start-Sleep $exposure_time_seconds
+
+            Close-Shutter $port $close_relay_hex
+
+            if ($number_of_pictures -gt 0) {
+                Start-Sleep 10
+            }
+
+            $i++
+
+        }
+
+        Write-Host "Pictures taken: $i"
+        Write-Host "Exposure time minutes: $($i * $exposure_time_seconds / 60)"
+}
+
+finally {
+    # Always close shutter
+    $port = New-Object System.IO.Ports.SerialPort("COM$com_port_number", $baud_rate, "None", 8, "One")
+    Close-Shutter $port $close_relay_hex
+}
